3ef508ccceb6d1ecb6bea3b748cf22e8b6bcb30b
Mehdi Achour <machour@gmail.com>
2019-04-14 16:10:31 +0100

    Update mutex namespace

--- src/Mutex.php
+++ src/Mutex.php
@@ -45,14 +45,14 @@ use yii\helpers\Yii;
  * ]
  * ```
  *
- * @see \yii\mutex\Mutex
+ * @see \Yii\Mutex\Mutex
  * @see http://redis.io/topics/distlock
  *
  * @author Sergey Makinen <sergey@makinen.ru>
  * @author Alexander Zhuravlev <axelhex@gmail.com>
  * @since 2.0.6
  */
-class Mutex extends \yii\mutex\Mutex
+class Mutex extends \Yii\Mutex\Mutex
 {
     /**
      * @var int the number of seconds in which the lock will be auto released.
