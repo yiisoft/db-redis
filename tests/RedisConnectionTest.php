ec43bd1c9d252107d0c4c9b2f647b25a4bbd21a5
Mehdi Achour <machour@gmail.com>
2019-04-25 15:54:34 +0100

    Update Log namespace

--- tests/RedisConnectionTest.php
+++ tests/RedisConnectionTest.php
@@ -4,7 +4,7 @@ namespace yii\db\redis\tests;
 
 use Yiisoft\Arrays\ArrayHelper;
 use yii\helpers\Yii;
-use Yii\Log\Logger;
+use Yiisoft\Log\Logger;
 use yii\db\redis\Connection;
 use yii\db\redis\SocketException;
 
