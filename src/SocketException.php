0ec357f45ea4aa24052cd40b591bfda4d9a29772
Wilmer <terabytesoftw@gmail.com>
2022-01-24 14:03:32 -0300

    Fix exception.

--- src/SocketException.php
+++ src/SocketException.php
@@ -4,19 +4,9 @@ declare(strict_types=1);
 
 namespace Yiisoft\Db\Redis;
 
+use Exception;
 use Yiisoft\Db\Connection\ConnectionInterface;
-use Yiisoft\Db\Exception\Exception;
 
-/**
- * SocketException indicates a socket connection failure in {@see ConnectionInterface}.
- */
-class SocketException extends Exception
+final class SocketException extends Exception
 {
-    /**
-     * @return string the user-friendly name of this exception
-     */
-    public function getName(): string
-    {
-        return 'Redis Socket Exception';
-    }
 }
